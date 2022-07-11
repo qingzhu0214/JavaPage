3ee70f84fefe922b2593ff241c8c9d345406c156
liyaqi <qingzhu0214@126.com>
2022-07-11 21:08:48 +0800

    11

---
+++ generate.py
@@ -0,0 +1,24 @@
+import os
+
+file_name = "index.md"
+    
+file = open(file_name, 'w', encoding='utf-8')
+
+base = os.path.join("_posts", "八股文")
+
+def recur(rootDir):
+    for lists in os.listdir(rootDir): 
+        path = os.path.join(rootDir, lists)
+        if path.split('.')[-1] == 'md':
+            # - [C++](_posts/2020-9-24-cpp.md)
+            file.write("- [{}]({})".format(path.split('\\')[-1][0:-3], '/'.join(path.split('\\'))))
+            file.write('\n')
+        else:
+            file.write("### {}".format(path.split('\\')[-1]))
+            file.write('\n')
+        print(path)
+        if os.path.isdir(path) and path.split('\\')[-1] != "image": 
+            recur(path) 
+            
+recur(base)
+file.close()
